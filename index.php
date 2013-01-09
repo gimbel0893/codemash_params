17d608619a3c0101ea28a0b2b05a958dd558c4cd
Michael Gimbel <gimbel0893@gmail.com>
2013-01-09 11:30:00 -0500

    added db code

--- index.php
+++ index.php
@@ -5,11 +5,31 @@
 
         <script>
             $(document).ready(function(){
-                alert('JQuery Loaded');
+                $('.main').fadeIn('slow');
             });
         </script>
 
     </head>
     <body>
+
+        <div class="main">
+            <?
+                $db = mysqli_connect($_SERVER['RDS_HOSTNAME'], $_SERVER['RDS_USERNAME'], $_SERVER['RDS_PASSWORD'], $_SERVER['RDS_DB_NAME'], $_SERVER['RDS_PORT']);
+                $sql = 'SELECT * FROM users WHERE id = ?';
+                $dbh = $db->prepare($sql);
+                $id = (int)$_GET['id'];
+                $dbh->bind_param('i', $id);
+                if(!$dbh->execute())
+                {
+                    echo sprintf('Query Failed: %s', $dbh->error);
+                    die;
+                }
+                $row = $dbh->get_result()->fetch_assoc();
+            ?>
+            first_name: <?=$row['first_name']?>
+            <br />
+            last_name: <?=$row['last_name']?>
+        </div>
+
     </body>
 </html>
