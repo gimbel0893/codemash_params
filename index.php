f2f983345975f06a24747b7125210b9455f7b44b
Michael Gimbel <gimbel0893@gmail.com>
2013-01-09 11:22:05 -0500

    made basic param app

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/<?=$_SERVER['JQUERY_VERSION']?>/jquery.min.js"></script>
+
+        <script>
+            $(document).ready(function(){
+                alert('JQuery Loaded');
+            });
+        </script>
+
+    </head>
+    <body>
+    </body>
+</html>
